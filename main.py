0f5f7a4efa162c17ca9380f0d8f50a35c9058cc8
AjayAchugatla <ajayachugatla17@gmail.com>
2025-08-03 21:35:50 +0530

    Day 1,2 task completed

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from dotenv import load_dotenv
+from murf import Murf
+import os
+# import httpx
+load_dotenv()
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -12,3 +16,14 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/tts")
+async def text_to_speech(text: str = Form(...)):
+    client = Murf(
+        api_key=os.getenv("MURF_API_KEY")
+    )
+    res = client.text_to_speech.generate(
+        text=text,
+        voice_id="en-US-natalie",
+    )
+    return res.audio_file
